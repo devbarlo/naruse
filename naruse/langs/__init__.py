2111dad9e5a67302e863e957886f009acc30ef82
TeamKillerX <randigithub356@gmail.com>
2022-09-14 16:06:43 +0700

    Create __init__.py

---
+++ naruse/langs/__init__.py
@@ -0,0 +1,10 @@
+# This file is a part of < https://github.com/TeamKillerX/naruse/ >
+# PLease read the GNU Affero General Public License in
+# <https://www.github.com/TeamKillerX/naruse/blob/dev22/LICENSE/>.
+
+from naruse.langs.language import langs
+
+get_string = langs.get_string
+reload_strings = langs.reload_strings
+get_languages = langs.get_languages
+get_language = langs.get_language
